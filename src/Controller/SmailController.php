8e73aece3e2c5765f746a108d82015a43ff36642
tom-sugai <tom.sugai@gmail.com>
2022-06-28 11:20:53 +0900

    set transport for debug

--- src/Controller/SmailController.php
+++ src/Controller/SmailController.php
@@ -9,12 +9,32 @@ class SmailController extends AppController
 {
     public function index(){
         $this->autoRender = false;
+        echo "Here is EmailController";
+
+        /** load from config/app_loca.php
         Email::configTransport('gmail_ssl', [
         'className' => 'Smtp'  
         ]);
+        */
+        /** 
+        Email::configTransport('gmail', [
+            'host' => 'smtp.gmail.com',
+            'port' => 587,
+            'username' => 'my@gmail.com',
+            'password' => 'secret',
+            'className' => 'Smtp',
+            'tls' => true
+        ]);
+        */
 
-        echo "Here is EmailController";
-    
+        // set transport for debug
+        $transport = new DebugTransport();
+        $email->setTransport($transport);
+        $result = $email->from(['me@example.com' => 'My Site'])
+            ->to('you@example.com')
+            ->subject('About')
+            ->send('My message');
+        debug($result);
     }
 }
 ?>
