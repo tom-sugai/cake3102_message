cd4fc8c5efcfff165c0a751a85e0a5e2bc3afe0c
tom-sugai <tom.sugai@gmail.com>
2022-06-30 11:20:19 +0900

    create testBar()

--- tests/TestCase/View/Helper/ProgrssHelperTest.php
+++ tests/TestCase/View/Helper/ProgrssHelperTest.php
@@ -16,6 +16,11 @@ class ProgressHelperTest extends TestCase
 
     public function testBar()
     {
-    
+        $result = $this->Progress->bar(90);
+        $this->assertContains('width: 90%', $result);
+        $this->assertContains('progress-bar', $result);
+
+        $result = $this->Progress->bar(33.3333333);
+        $this->assertContains('width: 33%', $result);
     }
 }
