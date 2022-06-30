d14ffa7e38b43109ce53bf1a0e6ee77a2d5fcf52
tom-sugai <tom.sugai@gmail.com>
2022-06-30 11:10:30 +0900

    create TestCase ProgressHelperTest class

---
+++ tests/TestCase/View/Helper/ProgrssHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\ProgressHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\view;
+
+class ProgressHelperTest extends TestCase
+{
+    public function setUp()
+    {
+    
+    }
+
+    public function testBar()
+    {
+    
+    }
+}
