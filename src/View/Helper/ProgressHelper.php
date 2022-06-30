d9c89c2282022b41ec82e06613d997c7179a96a8
tom-sugai <tom.sugai@gmail.com>
2022-06-30 11:01:51 +0900

    change sprint -> sprintf

--- src/View/Helper/ProgressHelper.php
+++ src/View/Helper/ProgressHelper.php
@@ -8,7 +8,7 @@ class ProgressHelper extends Helper
     public function bar($value)
     {
         $width = round($value / 100, 2) * 100;
-        return sprint(
+        return sprintf(
             '<div class="progress-container">
                 <div class="progress-bar" style="width: %s%%"></div>
             </div>', $width);    
