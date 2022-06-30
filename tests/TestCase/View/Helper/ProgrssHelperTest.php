ce64615151eb75573b7312d237a12adb74db3075
tom-sugai <tom.sugai@gmail.com>
2022-06-30 11:36:50 +0900

    change view -> View

--- tests/TestCase/View/Helper/ProgrssHelperTest.php
+++ tests/TestCase/View/Helper/ProgrssHelperTest.php
@@ -3,15 +3,15 @@ namespace App\Test\TestCase\View\Helper;
 
 use App\View\Helper\ProgressHelper;
 use Cake\TestSuite\TestCase;
-use Cake\View\view;
+use Cake\View\View;
 
 class ProgressHelperTest extends TestCase
 {
     public function setUp()
     {
         parent::setUp();
-        $view = new View();
-        $this->Progress = new ProgressHelper($view);
+        $View = new View();
+        $this->Progress = new ProgressHelper($View);
     }
 
     public function testBar()
