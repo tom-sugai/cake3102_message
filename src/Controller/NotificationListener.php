9b639c32994d4d053e42428c84edd67ce148425e
Tom Sugai <tom.sugai@gmail.com>
2024-01-17 12:41:15 +0900

    add Emai template

--- src/Controller/NotificationListener.php
+++ src/Controller/NotificationListener.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Cake\Event\EventListenerInterface;
 use Cake\Controller\ComponentRegistry;
 use App\Controller\Component\SendMailComponent;
+use Cake\Mailer\Email;
 
 /**
  * Class NotificationListener
@@ -32,6 +33,24 @@ class NotificationListener implements EventListenerInterface
      */
     public function mailNotification($event, $message, $order)
     {
-        $this->Email->send($message, $order);
+        //$this->Email->send($message, $order);
+
+        // create Email
+        $email = new Email();
+
+        // send mail
+        $result = $email
+        ->setTemplate('welcom', 'default') // 'view template' 'layout template'
+        //->viewBuilder()->setTemplate('default', 'default')
+        ->emailFormat('html')
+        ->setTo('fumiko@svr.home.com')
+        ->setFrom('tom@fmva52.home.com')
+        ->setSubject('Mail test from Smail controller!!')
+        ->deliver($message)
+        ->viewVars(['order' => $order])
+        ->send($message);
+
+        //debug($result);
+    
     }
 }
